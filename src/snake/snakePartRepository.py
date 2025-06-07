94ec92032132058b961f0a553c966b909237feda
dmccoystephenson <dmccoystephenson@gmail.com>
2025-06-07 11:07:23 -0600

    refactor: update snake part management to use SnakePartRepository

---
+++ src/snake/snakePartRepository.py
@@ -0,0 +1,9 @@
+class SnakePartRepository:
+    def __init__(self):
+        self.snake_parts = []
+
+    def get_length(self):
+        return len(self.snake_parts)
+
+    def append(self, snake_part):
+        self.snake_parts.append(snake_part)
